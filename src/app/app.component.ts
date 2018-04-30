f724b80583bef6a039e63deed5c837e3e6aab4e5
Elvin Agaragimow <imejibuh@gmail.com>
2018-04-30 10:57:07 +0300

    Подключение стилей. Подготовка проекта закончена.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'mel-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mel';
+export class AppComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
 }
